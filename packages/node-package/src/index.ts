ff0a35ef32be34ae8c14d8ec0c14a92c6aa64b3e
Zeno Jiricek <airtonix@users.noreply.github.com>
2025-04-09 08:52:13 +0930

    Reproduction of issue/87

--- packages/node-package/src/index.ts
+++ packages/node-package/src/index.ts
@@ -1,3 +1,11 @@
+import z from "zod";
+
 export function add(a: number, b: number): number {
 	return a + b;
+}
+
+const NameSchema = z.string();
+type Name = z.infer<typeof NameSchema>;
+export function echo(name: Name) {
+	return `Hello ${name} from node-package!`;
 }
